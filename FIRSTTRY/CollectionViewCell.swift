6a5b826fc4bf8f0f318de49bd0bac85ae937f770
olivier mountou <olivier.mountou@laposte.net>
2019-10-05 19:56:56 +0100

    add cell

---
+++ FIRSTTRY/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  FIRSTTRY
+//
+//  Created by Olivier Mountou on 05/10/2019.
+//  Copyright © 2019 Olivier Mountou MT. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cityUIimageView: UIImageView!
+    
+}
