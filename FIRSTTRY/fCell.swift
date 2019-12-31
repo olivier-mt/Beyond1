a3ef5f1490e1b5875dde3ca9a16d9f3c5a04cdd2
olivier mountou <olivier.mountou@laposte.net>
2019-12-31 11:53:04 +0000

    Fix last pb

---
+++ FIRSTTRY/fCell.swift
@@ -0,0 +1,31 @@
+//
+//  fCell.swift
+//  FIRSTTRY
+//
+//  Created by Olivier Mountou on 31/12/2019.
+//  Copyright © 2019 Olivier Mountou MT. All rights reserved.
+//
+
+import UIKit
+
+class fCell: UITableViewCell {
+
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var id: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
