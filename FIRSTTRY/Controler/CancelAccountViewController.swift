33809d2f582dabcecb10922e4fd2f833ef977c06
olivier mountou <olivier.mountou@laposte.net>
2020-03-22 09:56:48 +0000

    SET CANCEL ACCOUNT

--- FIRSTTRY/Controler/CancelAccountViewController.swift
+++ FIRSTTRY/Controler/CancelAccountViewController.swift
@@ -11,18 +11,68 @@ import Firebase
 
 class CancelAccountViewController: UIViewController {
 
-    @IBAction func logOutButton(_ sender: Any) {
-    }
+    var user = Auth.auth().currentUser;
+     
+
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
 
         // Do any additional setup after loading the view.
     }
     
+    
+    func cancel(){
+        user?.delete { error in
+      if let error = error {
+        // An error happened.
+      } else {
+        // Account deleted.
+      }
+    } }
+    
+    func resignin(){
+        
+       
 
-    @IBAction func pressLogout(_ sender: Any) {
-
+        
+       
+        
     }
+    
+    
+    @IBAction func cancelPress(_ sender: Any) {
+        
+        let email = self.emailTextField.text!
+        let password = self.passwordTextField.text!
+        var credential: AuthCredential
+
+        credential = EmailAuthProvider.credential(withEmail: email, password: password)
+        
+        
+               let user = Auth.auth().currentUser
+        
+        user?.reauthenticate(with: credential)
+
+        let uid = Auth.auth().currentUser
+
+        cancel()
+        
+        UserDefaults.standard.removeObject(forKey: "usersignedin")
+                   
+                   self.dismiss(animated: true, completion: nil)
+        
+        
+
+         }
     /*
     // MARK: - Navigation
 
@@ -32,5 +82,7 @@ class CancelAccountViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+
 
 }
